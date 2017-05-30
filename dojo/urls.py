d210b6815a78aa4605575d04e4291e2a36047c54
seoaro <seoaro@gmail.com>
2017-05-30 11:53:47 +0800

    dojo.views.mysum multiple num

--- dojo/urls.py
+++ dojo/urls.py
@@ -1,8 +1,10 @@
 # dojo/urls.py
 from django.conf.urls import url
 from . import views
+
 urlpatterns = [
-    url(r'^sum/(?P<x>\d+)/$', views.mysum),
-    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/$', views.mysum),
-    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)/$', views.mysum),
+    #url(r'^sum/(?P<x>\d+)/$', views.mysum),
+    #url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/$', views.mysum),
+    #url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)/$', views.mysum),
+    url(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
 ]
