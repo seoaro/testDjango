91654feb43b1aa222fbf525f05a31e71dd8a5c0d
seoaro <seoaro@gmail.com>
2017-05-30 13:18:30 +0800

    FBV 함수기반 뷰

--- dojo/urls.py
+++ dojo/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     #url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)/$', views.mysum),
     url(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
     url(r'^hello/(?P<name>[ㄱ-힣]+)/(?P<age>\d+)/$', views.hello),
+    url(r'^list1/$', views.post_list1),
+    url(r'^list2/$', views.post_list2),
+    url(r'^list3/$', views.post_list3),
+    url(r'^list4/$', views.excel_download),
 ]
--- dojo/views.py
+++ dojo/views.py
@@ -1,5 +1,7 @@
 # dojo/views.py
-from django.http import HttpResponse
+import os
+from django.conf import settings
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 #def mysum(request, x, y=0, z=0):
@@ -14,3 +16,29 @@ def mysum(request, numbers):
 
 def hello(request, name, age):
     return HttpResponse('Hi, {}. {} years old.'.format(name, age))
+
+def post_list1(request):
+    name = '공유'
+    return HttpResponse('''
+        <h1>AskDjango</h1>
+        <p>{name}</p>
+        <p>여러분의 파이썬&장고 페이스메이커가 되겠습니다.</p>'''.format(name=name))
+
+def post_list2(request):
+    name = '공유'
+    response = render(request, 'myapp/post_list.html', {'name': name})
+    return response
+
+def post_list3(request): #'FBV: JSON 형식 응답하기'
+    return JsonResponse({
+        'message': '안녕, 파이썬&장고',
+        'items': ['파이썬', '장고', 'Celery', 'Azure', 'AWS'], }, json_dumps_params={'ensure_ascii': False})
+
+def excel_download(request):
+#filepath = '/other/path/excel.xls'
+    filepath = os.path.join(settings.BASE_DIR, 'gdplev.xls')
+    filename = os.path.basename(filepath)
+    with open(filepath, 'rb') as f:
+        response = HttpResponse(f, content_type='application/vnd.ms-excel') # 필요한 응답헤더 세팅
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+        return response
