a0b29fe4c4083190f1ec7947554a346791009aed
seoaro <seoaro@gmail.com>
2017-05-30 11:41:16 +0800

    Dojo.views.mysum x/y/z args

--- dojo/views.py
+++ dojo/views.py
@@ -1,3 +1,8 @@
+# dojo/views.py
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def mysum(request, x, y=0, z=0):
+    # request: HttpRequest
+    return HttpResponse(int(x) + int(y) + int(z))
+
